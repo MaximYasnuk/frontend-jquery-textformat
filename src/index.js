9fc8db767431a11dfca4f2b2f0b85698cddabb8b
MaximYasnuk <maximyasnuk@gmail.com>
2017-07-20 01:37:25 +0300

    Working version

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 'use strict';
+var messageField = $('.message-form > .message-field');
+var messageFormatted = $('main > .message-formatted');
+var messageForm = $('main > .message-form');
 
-console.log($)
+function getFormattedText() {
+    messageFormatted.text(
+    messageField.val()
+        .toLowerCase()
+        .trim()
+        .replace(/\s+/g,' ')
+    )
+}
+function reset() {
+    messageFormatted.text('');
+}
+
+messageField.on('keyup', getFormattedText);
+messageForm.on('reset', reset);
